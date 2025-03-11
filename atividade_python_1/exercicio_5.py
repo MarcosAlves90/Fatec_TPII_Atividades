7b786e12e1cec27a91263237283d3ccea53a2e5c
MarcosAlves90 <MarcosAlves25@proton.me>
2025-03-10 23:21:22 -0300

    Refactor variable names and improve function readability
    Renamed variables and functions for better readability and consistency. Simplified naming conventions to adhere to concise and clear standards. Enhanced overall code clarity without altering functionality.

--- atividade_python_1/exercicio_5.py
+++ atividade_python_1/exercicio_5.py
@@ -1,61 +1,61 @@
 import random, os
 
 opcoes = {1: "pedra", 2: "papel", 3: "tesoura"}
-score_humano = 0
-score_maquina = 0
-total_empates = 0
-resultado_final = ""
+scoreH = 0
+scoreM = 0
+empate = 0
+result = ""
 
-def comparar_jogadas(jogada_humano, jogada_maquina, score_humano, score_maquina, total_empates):
-    if jogada_humano == jogada_maquina:
-        mensagem = "Empate"
-        total_empates += 1
-    elif jogada_maquina == 1:
-        if jogada_humano == 2:
-            mensagem = "Você venceu!"
-            score_humano += 1
+def comparador(humano, maquina, hum_score, maq_score, empate_score):
+    if humano == maquina:
+        msg = "empate"
+        empate_score += 1
+    elif maquina == 1:
+        if humano == 2:
+            msg = "Você venceu!"
+            hum_score += 1
         else:
-            mensagem = "A máquina venceu!"
-            score_maquina += 1
-    elif jogada_maquina == 2:
-        if jogada_humano == 1:
-            mensagem = "A máquina venceu!"
-            score_maquina += 1
+            msg = "A máquina venceu!"
+            maq_score += 1
+    elif maquina == 2:
+        if humano == 1:
+            msg = "A máquina venceu!"
+            maq_score += 1
         else:
-            mensagem = "Você venceu!"
-            score_humano += 1
-    elif jogada_maquina == 3:
-        if jogada_humano == 1:
-            mensagem = "Você venceu!"
-            score_humano += 1
+            msg = "Você venceu!"
+            hum_score += 1
+    elif maquina == 3:
+        if humano == 1:
+            msg = "Você venceu!"
+            hum_score += 1
         else:
-            mensagem = "A máquina venceu!"
-            score_maquina += 1
-    return mensagem, score_humano, score_maquina, total_empates
+            msg = "A máquina venceu!"
+            maq_score += 1
+    return msg, hum_score, maq_score, empate_score
 
-def obter_jogada_humano():
+def jogada_humano():
     jogada = input("Digite o seu ataque: ")
     while jogada != str(1) and jogada != str(2) and jogada != str(3):
-        jogada = input("\nUtilize um dos valores válidos (1, 2 ou 3).\nDigite novamente: ")
+        jogada = input("\nUtilize um dos valores válidos (1, 2  ou 3).\nDigite novamente: ")
     return int(jogada)
 
-def jogar(score_humano, score_maquina, total_empates, resultado_final):
-    for rodada in range(1, 11):
+def jogar(pontua_hum, pontua_maq, ponto_empate, resultado):
+    for x in range(1, 11):
         os.system('cls')
-        print(f"===== JO KEN PO =====\n[[ Rodada {rodada} de 10 ]]\n")
+        print(f"----- JO KEN PO -----\n>>> Rodada {x} de 10 <<<")
         print("[1] Pedra\n[2] Papel\n[3] Tesoura\n")
-        jogada_humano = obter_jogada_humano()
-        jogada_maquina = random.randint(1, 3)
-        print(f'\nVocê jogou {opcoes[jogada_humano]}\nA máquina jogou {opcoes[jogada_maquina]}\n')
-        resultado_final, score_humano, score_maquina, total_empates = comparar_jogadas(jogada_humano, jogada_maquina, score_humano, score_maquina, total_empates)
-        print(f'{resultado_final}\nSeu score: {score_humano}\nScore da máquina: {score_maquina}\nEmpates: {total_empates}\n')
+        jogada_h = jogada_humano()
+        jogada_m = random.randint(1, 3)
+        print(f'\nVocê jogou {opcoes[jogada_h]}\nA máquina jogou {opcoes[jogada_m]}\n')
+        resultado, pontua_hum, pontua_maq, ponto_empate = comparador(jogada_h, jogada_m, pontua_hum, pontua_maq, ponto_empate)
+        print(f'{resultado}\nSeu score: {pontua_hum}\nScore da máquina: {pontua_maq}\nEmpates: {ponto_empate}\n')
         input(f'[PRESSIONE ENTER PARA CONTINUAR]')
-    return score_humano, score_maquina, total_empates, resultado_final
+    return pontua_hum, pontua_maq, ponto_empate, resultado
 
-score_humano, score_maquina, total_empates, resultado_final = jogar(score_humano, score_maquina, total_empates, resultado_final)
+scoreH, scoreM, empate, result = jogar(scoreH, scoreM, empate, result)
 while True:
     continuar = input("Quer continuar jogando? [S/N]: ")
     if continuar.upper() == "S":
-        score_humano, score_maquina, total_empates, resultado_final = jogar(score_humano, score_maquina, total_empates, resultado_final)
+        scoreH, scoreM, empate, result = jogar(scoreH, scoreM, empate, result)
     else:
         break
